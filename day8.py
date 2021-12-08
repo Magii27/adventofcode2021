9fdb63816af667905fed4345c4a234067044a820
Magii27 <80420368+Magii27@users.noreply.github.com>
2021-12-08 21:04:55 +0100

    Add files via upload

--- day8.py
+++ day8.py
@@ -40,7 +40,7 @@ b    .  b    .  .    c  b    c  b    c     1    2
 .    f  e    f  .    f  e    f  .    f     4    5
  gggg    gggg    ....    gggg    gggg       6666
  """
-segment_num_part2 = {0:[0, 1, 2, 4, 5, 6], 1:[2, 5], 2:[0, 2, 3, 4, 6], 3:[0, 2, 3, 4, 6],
+segment_num_part2 = {0:[0, 1, 2, 4, 5, 6], 1:[2, 5], 2:[0, 2, 3, 4, 6], 3:[0, 2, 3, 5, 6],
                    4:[1, 2, 3, 5], 5:[0, 1, 3, 5, 6], 6:[0, 1, 3, 4, 5, 6], 7:[0, 2, 5],
                    8:[0, 1, 2, 3, 4, 5, 6], 9:[0, 1, 2, 3, 5, 6]}
 
@@ -56,6 +56,7 @@ for i  in range(len(signal_patterns)):
     signal_patterns[i] = list(sorted(signal_patterns[i], key=len))
 
 print(signal_patterns)
+print(output_value)
 
 def get_pattern(signal_pattern, segment_list, segment_list3):
     segment_list2 = segment_list3.copy()
@@ -81,8 +82,8 @@ def get_pattern(signal_pattern, segment_list, segment_list3):
                     continue
                 to_add_letter.append(letter)
         elif i == 9:
-            to_add_index.append(4)
             to_add_index.append(6)
+            to_add_index.append(4)
             for letter in signal_pattern[i]:
                 if letter in to_add_letter:
                     continue
@@ -97,15 +98,18 @@ def get_pattern(signal_pattern, segment_list, segment_list3):
 
 ergebnis = 0
 for i in range(len(output_value)):
-    hochzaehlen = ""
+    #hochzaehlen = ""
     vergleich = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
     segment_list1 = segment_clear.copy()
     segment_list = get_pattern(signal_patterns[i], segment_num_part2, segment_list1)
     print(segment_list)
 
     for item_key in output_value.keys():
+        hochzaehlen = ""
         for list_value in output_value[item_key]:
+            #hochzaehlen = ""
             breakout = False
+            print("länge: ", len(list_value))
             if 2 == len(list_value):
                 hochzaehlen += "1"
             elif len(list_value) == 4:
@@ -114,23 +118,49 @@ for i in range(len(output_value)):
                 hochzaehlen += "7"
             elif len(list_value) == 7:
                 hochzaehlen += "8"
+                #print(hochzaehlen)
             else:
-                for letter_value in list_value:
-                    pruefung_letter = False
-                    for key_segment in range(0, 10):
+                print("else")
+                for key_segment in range(0, 10):
+                    all_time_count = 0
+                    length = (segment_list[key_segment])
+                    count2 = 0
+                    for letter_value in list_value:
                         count = 0
-                        if len(segment_list[key_segment]) != len(list_value):
+                        for item_segment_letter in segment_list[key_segment]:
+                            if letter_value == item_segment_letter:
+                                print("Buchstabe ist drinne: ", item_segment_letter)
+                                count += 1
+                                segment_w = key_segment
+                                count2 += 1
+                                break
+                        else:
+                            print("nicht gefunden")
+                            count = 0
+                            count2 += 1
+                            break
+
+                        all_time_count += count
+                        #print("count: ", all_time_count, "count2: ", count2, len(segment_list[segment_w]))
+                        if all_time_count == len(list_value) and count2 == len(segment_list[segment_w]):
+                            #print("vor hochzaehlen: ", hochzaehlen, segment_w)
+                            hochzaehlen += str(segment_w)
+                            print("hochzaehlen: ", hochzaehlen)
                             breakout = True
                             break
-                        else:
-                            for item_segment_letter in segment_list[key_segment]:
-                                if letter_value == item_segment_letter:
-                                    count += 1
-                                    break
-                            else:
-                                count -= 1
                     if breakout is True:
+                        print("break")
                         break
+
+                       #print("all time count: ", all_time_count, "len: ", len(list_value))
+                       #if count == len(list_value):
+                       #    print("segfment beim hochz: ", str(segment_w))
+                       #    hochzaehlen += str(segment_w)
+                       #if breakout is True:
+                       #    break
+
+
+            #print(hochzaehlen)
     ergebnis += int(hochzaehlen)
 
 
